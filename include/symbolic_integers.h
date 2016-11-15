8d0cc4c6ca228ab1c8eebe559ed78aec80d69124
Botev <botevmg@gmail.com>
2016-11-15 14:37:46 +0000

    Small convention change

--- include/symbolic_integers.h
+++ include/symbolic_integers.h
@@ -21,16 +21,16 @@ namespace md{
     }
 }
 
-#include "base_monomial.h"
-#include "base_polynomial.h"
-#include "base_registry.h"
+#include "monomial.h"
+#include "polynomial.h"
+#include "registry.h"
 #include "monomial_ops.h"
 #include "polynomial_ops.h"
 #include "extra_ops.h"
 
-#include "impl/base_monomial_impl.h"
-#include "impl/base_polynomial_impl.h"
-#include "impl/base_registry_impl.h"
+#include "impl/monomial_impl.h"
+#include "impl/polynomial_impl.h"
+#include "impl/registry_impl.h"
 #include "impl/monomial_ops_impl.h"
 #include "impl/polynomial_ops_impl.h"
 #include "impl/extra_ops_impl.h"
